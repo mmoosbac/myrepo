f092c2cca0913c41eaf579d71f6db4f4e95fc165
mmoosbac <>
2014-10-28 10:44:22 +0100

    myrepo - add first files

---
+++ employee/src/employee/Employee.java
@@ -0,0 +1,16 @@
+package employee;
+
+public class Employee {
+	
+	String firstName;
+	String lastName;
+	
+	Integer ID;
+	
+	void printFullName(String first, String last) {
+		
+	
+		System.out.println(first + " " + last);
+	}	
+		
+}
